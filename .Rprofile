01bc73b0682e0abc47cefc95facc11b327afae9c
Martin Gleason <marty.data.scientist@icloud.com>
2025-02-23 12:38:52 -0600

    blastula
    successful test of blastula with button and a pic of schatzi

--- .Rprofile
+++ .Rprofile
@@ -1,1 +1,6 @@
 source("renv/activate.R")
+
+Sys.setenv(
+  INFO = "info@organizers.science",
+  PRIVATE_EMAIL_PASS = "DUX@evu4vky5tpk2bxk"
+)
---
+++ scripts/blastula_experiment.R
@@ -0,0 +1,76 @@
+
+# purpose -----------------------------------------------------------------
+
+#creating and sending emails with blastula
+#note on system.file(): this function pulls from a package. Since Schatzi is
+#in a different directory, I can use here()
+
+# packages ----------------------------------------------------------------
+
+library(blastula)
+library(here)
+library(glue)
+
+image_path <- here("pics", "schatzi_approve.jpeg")
+
+pic <- add_image(file = image_path)
+
+btn <- add_cta_button(
+  url = "https://organizers.science",
+  text = "r4Revolution"
+)
+
+test <- compose_email(
+  body = md(glue(    
+  "
+Hello friend,
+    
+I am sending you an email from an R package called **blastula**.
+    
+This is a version sent from an R script.
+
+This email is approved by Schatzi.
+
+{pic}
+    
+Enjoy,<br>
+Marty
+  {btn}"
+  )  
+)
+)
+test
+
+
+# credentials -------------------------------------------------------------
+
+private_email_creds <- creds_envvar(
+  user = Sys.getenv("INFO"),
+  pass_envvar = "PRIVATE_EMAIL_PASS",
+  host = "mail.privateemail.com",
+  port = 465,
+  use_ssl = TRUE
+  )
+
+
+# sending email via smtp --------------------------------------------------
+
+test_emails <- c("Marty Gleason" = "martin.gleason.ms@gmail.com", 
+                 "Kylah Seltzer Johnston" = "kylahrsj@gmail.com",
+                 "Shannon Erin Glass" = "Shannonerin@gmail.com")
+
+test %>%
+  smtp_send(
+    to = test_emails,
+    from = Sys.getenv("INFO"),
+    subject ="Test Email, Approved by Schatzi" ,
+    credentials = private_email_creds
+  )
+
+
+
+# Next steps --------------------------------------------------------------
+
+# pull first line from .qmd
+
+  
