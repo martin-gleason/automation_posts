df0b0eec9d8f526b4c3358693cd55c53fc2872f6
Martin Gleason <marty.data.scientist@icloud.com>
2025-03-03 17:20:31 -0600

    email
    test two, seeting up additinoal tet statements

--- scripts/creds.R
+++ scripts/creds.R
@@ -1,7 +1,7 @@
 
 private_email_creds <- blastula::creds_envvar(
   user = Sys.getenv("EMAIL"),
-  pass_envvar = "PASSWORD",
+  pass_envvar = Sys.getenv("PASSWORD"),
   host = "mail.privateemail.com",
   port = 465,
   use_ssl = TRUE
